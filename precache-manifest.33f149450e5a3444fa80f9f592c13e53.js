8aebac8ba4033a4ab826ddbb81747865529bbe5b
eunicLab <s1787807@FVFTXNHCJ1WKs-MacBook-Air.local>
2019-12-22 00:40:54 +0100

    Updates

---
+++ precache-manifest.33f149450e5a3444fa80f9f592c13e53.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a2bb664873a78b8d1d359fd8d1da2bc5",
+    "url": "/docsite/index.html"
+  },
+  {
+    "revision": "8a53f5d7b81e84163604",
+    "url": "/docsite/static/css/main.0af2a151.chunk.css"
+  },
+  {
+    "revision": "cfd526c14369408f5284",
+    "url": "/docsite/static/js/2.b2a2c858.chunk.js"
+  },
+  {
+    "revision": "8a53f5d7b81e84163604",
+    "url": "/docsite/static/js/main.6f1eaee7.chunk.js"
+  },
+  {
+    "revision": "0ed4bb570e00245e0479",
+    "url": "/docsite/static/js/runtime-main.f30fe001.js"
+  },
+  {
+    "revision": "9e4259ff601229be2c2466c41636a4d6",
+    "url": "/docsite/static/media/Hospital.9e4259ff.jpg"
+  },
+  {
+    "revision": "129fc8bf95fde39933a5c3b8e359b500",
+    "url": "/docsite/static/media/Wheelchair.129fc8bf.jpg"
+  },
+  {
+    "revision": "d175dba2a6a40c39d9e54e5db52ac3ad",
+    "url": "/docsite/static/media/Wheelchair2.d175dba2.jpg"
+  },
+  {
+    "revision": "1304fdacd75b35cdbe8ceef135c5d1ce",
+    "url": "/docsite/static/media/backwardArrow.1304fdac.svg"
+  },
+  {
+    "revision": "2de0905bddb887e3b9562d9c2e418436",
+    "url": "/docsite/static/media/forwardArrow.2de0905b.svg"
+  },
+  {
+    "revision": "bd4a1055db51b0e4a2f97e9bbdd0bdb3",
+    "url": "/docsite/static/media/frontdesk.bd4a1055.jpg"
+  },
+  {
+    "revision": "780ff31f777b451745c84731b757fb1b",
+    "url": "/docsite/static/media/intensive.780ff31f.jpg"
+  },
+  {
+    "revision": "cdbca3d3c4574afd0b879094e1fc5040",
+    "url": "/docsite/static/media/lab.cdbca3d3.jpg"
+  },
+  {
+    "revision": "e307a0f3c0056dfa51ac7a90e5c07da4",
+    "url": "/docsite/static/media/lab2.e307a0f3.jpg"
+  },
+  {
+    "revision": "740e304a1f946498fde3548de29ad3be",
+    "url": "/docsite/static/media/laboratory.740e304a.jpg"
+  },
+  {
+    "revision": "83f907c7debe26493ecd6879d59e0c15",
+    "url": "/docsite/static/media/massage.83f907c7.jpg"
+  },
+  {
+    "revision": "a970ff3e0a9b9f50aac0c20661f79806",
+    "url": "/docsite/static/media/massage2.a970ff3e.jpg"
+  },
+  {
+    "revision": "a80956e6d780e76d03afd8773944d017",
+    "url": "/docsite/static/media/staff.a80956e6.jpg"
+  },
+  {
+    "revision": "c01f9d889e845ea73797749a56579872",
+    "url": "/docsite/static/media/surgery1.c01f9d88.jpg"
+  },
+  {
+    "revision": "2f0fa54eda7a22f42f474fa861c2a16b",
+    "url": "/docsite/static/media/surgery2.2f0fa54e.jpg"
+  },
+  {
+    "revision": "bbd47281049628ac3a8f687f3339c05d",
+    "url": "/docsite/static/media/white-background.bbd47281.png"
+  }
+]);
